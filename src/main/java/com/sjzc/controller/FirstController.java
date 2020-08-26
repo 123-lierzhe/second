960c15ececab7d2f6f0b529460c90a19bba98bcf
lierzhe <lierzhezzz@163.com>
2020-08-26 17:48:23 +0800

    hahahah

--- src/main/java/com/sjzc/controller/FirstController.java
+++ src/main/java/com/sjzc/controller/FirstController.java
@@ -1,5 +1,8 @@
 package com.sjzc.controller;
 
 public class FirstController {
-
+	public static void main(String[] args) {
+		System.out.println("ssss");
+		System.out.println("sssssssssss");
+	}
 }
